57593f5c6fa3193d1feaa52cc117af25672dd7b7
Lanse <simon_stalnabb@hotmail.com>
2020-02-16 19:02:45 +0100

    move deferred registry

--- src/main/java/lanse505/essence/utils/EssenceRegistration.java
+++ src/main/java/lanse505/essence/utils/EssenceRegistration.java
@@ -16,7 +16,10 @@ import net.minecraftforge.registries.ForgeRegistry;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EssenceRegistration {
+    private static ForgeRegistry<Modifier> MODIFIER_REGISTRY = null;
+    private static final DeferredRegister<Modifier> MODIFIER_DEFERRED_REGISTER = new DeferredRegister<>(MODIFIER_REGISTRY, EssenceReferences.MODID);
     private static final DeferredRegister<Feature<?>> FEATURE_DEFERRED_REGISTER = new DeferredRegister<>(ForgeRegistries.FEATURES, EssenceReferences.MODID);
+
     public static RegistryObject<NormalEssenceTreeFeature> NORMAL_ESSENCE_TREE_FEATURE = FEATURE_DEFERRED_REGISTER.register("normal_essence_tree",
             () -> new NormalEssenceTreeFeature(TreeFeatureConfig::deserialize));
     public static RegistryObject<FancyEssenceTreeFeature> FANCY_ESSENCE_TREE_FEATURE = FEATURE_DEFERRED_REGISTER.register("fancy_essence_tree",
@@ -25,8 +28,7 @@ public class EssenceRegistration {
             AttackDamageModifier::new);
     public static RegistryObject<ExpanderModifier> EXPANDER_MODIFIER = MODIFIER_DEFERRED_REGISTER.register("expander_modifier",
             ExpanderModifier::new);
-    private static ForgeRegistry<Modifier> MODIFIER_REGISTRY;
-    private static final DeferredRegister<Modifier> MODIFIER_DEFERRED_REGISTER = new DeferredRegister<>(MODIFIER_REGISTRY, EssenceReferences.MODID);
+
 
     public static void register(IEventBus eventBus) {
         FEATURE_DEFERRED_REGISTER.register(eventBus);
